05daa7cdd2a289ce9aff2bbc7f849b31340f9b69
Emil Nilsson <emil.nilsson@protonmail.com>
2019-11-16 17:37:47 +0100

    Convert to typescript

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-const express = require('express')
+import express from 'express'
+
 const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-	res.send('Hello, world!')
+  res.send('Hello, world!')
 })
 
 app.listen(port, () => {
-	console.log(`Server started at: http://localhost:${port}`)
+  console.log(`Server started at: http://localhost:${port}`)
 })
